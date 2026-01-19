46aa301da6307c2010bf81c84264a3fd04964966
Bryan Descours <bryan.delianov@gmail.com>
2026-01-19 15:56:04 +0100

    Mise à jour de la classe Contrat.cs

--- Models/Contrat.cs
+++ Models/Contrat.cs
@@ -8,5 +8,10 @@ namespace LocAutoPlusApp.Models
 {
     public class Contrat
     {
+        public int UserId { get; }
+        public DateTime DateDebut { get; }
+        public DateTime DateFin { get; }
+        public decimal Montant { get; }
+        public string EtatContrat { get; }
     }
 }
