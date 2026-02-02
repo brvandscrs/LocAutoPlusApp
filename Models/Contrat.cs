0eb2dac59f03f19307f93e2910155f62ed8ebcdb
Bryan Descours <bryan.delianov@gmail.com>
2026-02-02 10:28:01 +0100

    Mise à jour de Contrat.cs et MainWindow.xaml

--- Models/Contrat.cs
+++ Models/Contrat.cs
@@ -6,6 +6,15 @@
         public DateTime DateDebut { get; }
         public DateTime DateFin { get; }
         public decimal Montant { get; }
-        public string EtatContrat { get; }
+        public string EtatContrat { get; set; }
+
+        public Contrat(int userId, DateTime dateDebut, DateTime dateFin, decimal montant, string etatContrat)
+        {
+            UserId = userId;
+            DateDebut = dateDebut;
+            DateFin = dateFin;
+            Montant = montant;
+            EtatContrat = etatContrat;
+        }
     }
 }
