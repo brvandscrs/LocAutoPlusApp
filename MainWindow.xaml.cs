d041b6e4f905dde2b5a64d4b6925770bff51d9fe
Bryan Descours <bryan.delianov@gmail.com>
2026-03-02 12:24:40 +0100

    Remplace ListeContratView par ContratListingView + VM
    Remplacement de la vue ListeContratView par ContratListingView dans MainWindow. Ajout du ViewModel ContratListingViewModel avec gestion de la sélection et notification des propriétés. Suppression des using inutiles dans plusieurs fichiers .cs.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using LocAutoPlusApp.ViewModels;
-using System.Windows;
+﻿using System.Windows;
 
 namespace LocAutoPlusApp
 {
---
+++ ViewModels/ContratListingViewModel.cs
@@ -0,0 +1,26 @@
+﻿using LocAutoPlusApp.Models;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace LocAutoPlusApp.ViewModels
+{
+    internal class ContratListingViewModel : INotifyPropertyChanged
+    {
+        private Contrat? _selectedContrat;
+        public ObservableCollection<Contrat> Contrats { get; set; }
+        public Contrat SelectedContrat
+        {
+            get => _selectedContrat; 
+            set
+            {
+                _selectedContrat = value;
+                OnPropertyChanged(); // Uncomment if BaseViewModel implements INotifyPropertyChanged
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
