c9f0dc98aa00eb6dde14f6fdea92cb14a19e9695
Bryan Descours <bryan.delianov@gmail.com>
2026-05-22 16:34:03 +0200

    Mise à jour des identifiants de connexion à la BDD.

--- Helpers/DatabaseHelper.cs
+++ Helpers/DatabaseHelper.cs
@@ -9,7 +9,7 @@ namespace LocAutoPlusApp.Helpers
     public static class DatabaseHelper
     {
         private const string ConnectionString =
-            "Server=localhost;Port=3306;Database=sc2kuph3194_locautoplus;Uid=sc2kuph3194_locautoplus;Pwd=iLkYBVmKxUM5Vwhs;CharSet=utf8mb4;";
+            "Server=localhost;Port=3306;Database=sc2kuph3194_locautoplus;Uid=sc2kuph3194_locautoplus;Pwd=juD3xxSDUlpjNdnR;CharSet=utf8mb4;";
 
         public static MySqlConnection GetConnection()
         {
